6754f8a1966b42926c722d8e90442f2586af2668
Bartosz Nowak <batek.nowak@yahoo.pl>
2021-06-20 13:23:20 +0200

    Add build script

--- run.sh
+++ run.sh
@@ -1,34 +1,46 @@
 #!/bin/bash
 set -e
 
-(
-    cd "./../zmk/app" || return
+DEFAULT_REPO_PATH="../zmk/app"
 
-    while [[ $# -ge 0 ]]
-    do
+LEFT=false
+RIGHT=false
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]; do
     key="$1"
+
     case $key in
-        -l|--left)
-        echo "building left"
-        west build -p always -b particle_xenon -- -DSHIELD=dactyl_manuform_left -DZMK_CONFIG="$PWD/../../zmk-config/config"
-        shift
+    -l | --left)
+        LEFT=true
+        shift # past arg
         ;;
-
-        -r|--right)
-        echo "building right"
-        west build -p always -b particle_xenon -- -DSHIELD=dactyl_manuform_right -DZMK_CONFIG="$PWD/../../zmk-config/config"
-        shift
+    -r | --right)
+        RIGHT=true
+        shift # past arg
         ;;
-
-        *)
-        # unknown option
-        echo "Please define proper board site"
-        exit 1
+    *)                     # unknown option
+        POSITIONAL+=("$1") # save it in an array for later
+        shift              # past argument
         ;;
     esac
-    done
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
 
-    ### install over usb
+(
+    cd $DEFAULT_REPO_PATH
+
+    if $LEFT; then
+        echo "building left"
+        west build -p always -b particle_xenon -- -DSHIELD=dactyl_manuform_left -DZMK_CONFIG="$PWD/../../zmk-config/config"
+    elif $RIGHT; then
+        echo "building right"
+        west build -p always -b particle_xenon -- -DSHIELD=dactyl_manuform_right -DZMK_CONFIG="$PWD/../../zmk-config/config"
+    else
+        echo "Please define proper board site"
+        exit 1
+    fi
+
     rm -rf dfu-package.zip
     adafruit-nrfutil dfu genpkg --dev-type 0x0052 --application build/zephyr/dactyl_manuform*.hex dfu-package.zip
     adafruit-nrfutil dfu serial --package dfu-package.zip -p /dev/ttyACM* -b 115200
