a34ebb057d7f4b18779cc7701f1ebc7ba3a8ed53
dumam <batek.nowak@yahoo.pl>
2021-09-11 22:18:04 +0200

    Fix logging

--- log.py
+++ log.py
@@ -3,16 +3,18 @@
 import pyudev
 import pathlib
 import subprocess
-
-def devNode(device: pyudev.Device):
-    print(device.device_node)
-    print(device.device_path)
     
-    if not device.parent:
-        return
-    else:
-        devNode(device.parent)
+def getDevPath(device: pyudev.Device):
+    # I can add more logic here, to run different scripts for different devices.
+    devicePath = pathlib.Path(device.sys_path)
+    if not (devicePath / 'tty').exists():
+        return ""
     
+    devicePaths = devicePath.rglob("ttyACM*")
+    for ttyACM in devicePaths:
+        return '/dev/' + ttyACM.stem
+    
+    return ""
 
 def main():
     context = pyudev.Context()
@@ -27,17 +29,12 @@ def main():
             
             if device.action != "add":
                 break
-            
-            # I can add more logic here, to run different scripts for different devices.
-            devicePath = pathlib.Path(device.sys_path)
-            if not (devicePath / 'tty').exists():
-                break
-            
-            devicePaths = devicePath.rglob("ttyACM*")
-            for ttyACM in devicePaths:
-                cmd = ['picocom', '-b', '115200', '/dev/' + ttyACM.stem]
-                print("Executed {}".format(" ".join(cmd)))
-                return subprocess.run(cmd)
+        
+            devPath = getDevPath(device)
+            cmd = ['picocom', '-b', '115200', devPath]
+            print("Executed {}".format(" ".join(cmd)))
+            return subprocess.run(cmd)
+
 
 if __name__ == '__main__':
     main()
